e9991349c1c9b3569feb351a4c720f25ebd41968
adel <adeelguitoun@gmail.com>
2024-10-23 12:08:56 +0100

    Add unit test for button component

---
+++ packages/mint-ui/src/Button/Button.test.tsx
@@ -0,0 +1,12 @@
+import {  render, screen } from '@testing-library/react';
+import {  describe, expect, it } from 'vitest';
+
+import { Button } from './Button';
+
+describe("First test", () => {
+  it("should have the role button", () => {
+    render(<Button />)
+    expect(screen.getByRole('button')).toHaveAttribute("type", "button")
+  })
+})
+
--- packages/mint-ui/src/Button/Button.tsx
+++ packages/mint-ui/src/Button/Button.tsx
@@ -1,5 +1,5 @@
 export const Button = () => {
   return (
-    <button className="bg-red-500 px-3 py-2 rounded-xl text-white font-semibold text-md">Button</button>
+    <button className="bg-red-500 px-3 py-2 rounded-xl text-white font-semibold text-md" type="button">Button</button>
   )
 }
