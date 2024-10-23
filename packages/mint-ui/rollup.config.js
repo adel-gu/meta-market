2c8ab5e69879a0ca0b9ce54aa4739a3267f814bb
adel <adeelguitoun@gmail.com>
2024-10-23 17:39:41 +0100

    Update rollup dist and add release object to nx

--- packages/mint-ui/rollup.config.js
+++ packages/mint-ui/rollup.config.js
@@ -5,7 +5,7 @@ const svg = require('@svgr/rollup');
 module.exports = withNx(
   {
     main: './src/index.ts',
-    outputPath: '../../dist/packages/mint-ui',
+    outputPath: './dist',
     tsConfig: './tsconfig.lib.json',
     compiler: 'babel',
     external: ['react', 'react-dom', 'react/jsx-runtime'],
