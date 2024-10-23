b1644f5fa06615e4fddb5520783b30eea95a4d63
adel <adeelguitoun@gmail.com>
2024-10-23 11:32:13 +0100

    Configure storybook and add button component stories

---
+++ packages/mint-ui/src/Button/Button.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
+
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Button',
+};
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary:Story = {
+  args: {},
+};
