0afa2a8ef2fe15da51b72ea5509c32f311adc6e5
adel <adeelguitoun@gmail.com>
2024-10-23 22:35:41 +0100

    Update the build configurations

--- packages/mint-ui/rollup.config.js
+++ packages/mint-ui/rollup.config.js
@@ -2,15 +2,22 @@ const { withNx } = require('@nx/rollup/with-nx');
 const url = require('@rollup/plugin-url');
 const svg = require('@svgr/rollup');
 
+const entry = "./src/index.ts"
+const outDir = "./dist"
+
 module.exports = withNx(
   {
-    main: './src/index.ts',
-    outputPath: './dist',
+    main: entry,
+    outputPath: outDir,
     tsConfig: './tsconfig.lib.json',
-    compiler: 'babel',
+    compiler: 'tsc',
     external: ['react', 'react-dom', 'react/jsx-runtime'],
     format: ['esm'],
     assets: [{ input: '.', output: '.', glob: 'README.md' }],
+    project: "./package.json",
+    extractCss: "base.css",
+    sourceMap: true,
+    deleteOutputPath: true,
   },
   {
     // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
