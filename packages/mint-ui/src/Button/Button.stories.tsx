15c7db1ea32e511094661f995cc77f72222fb0b8
adel <adeelguitoun@gmail.com>
2024-11-04 11:07:02 +0100

    Re-configure shadcn

--- packages/mint-ui/src/Button/Button.stories.tsx
+++ packages/mint-ui/src/Button/Button.stories.tsx
@@ -1,16 +1,15 @@
-import { expect } from '@storybook/jest';
 import type { Meta, StoryObj } from '@storybook/react';
-import { within } from '@storybook/testing-library';
 
-import { Button } from './Button';
+import { Button } from './button';
 
 const meta: Meta<typeof Button> = {
 	component: Button,
-	title: 'Button',
 };
 export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
-	args: {},
+	args: {
+		children: 'Button',
+	},
 };
--- packages/mint-ui/src/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-export const Button = () => {
-	return (
-		<button
-			className='text-white rounded-xl bg-gradient-1 px-3 py-2 text-md font-semibold'
-			type='button'>
-			Button
-		</button>
-	);
-};
