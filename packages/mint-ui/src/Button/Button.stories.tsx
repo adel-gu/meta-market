a8da647f2a8d66a4fad92aa3b128d88ec970b0a8
adel <adeelguitoun@gmail.com>
2024-11-04 20:39:33 +0100

    Create Button component

--- packages/mint-ui/src/Button/Button.stories.tsx
+++ packages/mint-ui/src/Button/Button.stories.tsx
@@ -3,13 +3,39 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './button';
 
 const meta: Meta<typeof Button> = {
+	title: 'Components/Button',
 	component: Button,
+	parameters: {
+		layout: 'centered',
+	},
+	argTypes: {
+		asChild: {
+			options: [true, false],
+			control: { type: 'boolean' },
+		},
+		size: {
+			options: ['default', 'sm', 'lg'],
+			control: { type: 'inline-radio' },
+		},
+	},
+	args: {
+		asChild: false,
+		size: 'default',
+	},
+	tags: ['autodocs'],
 };
 export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
 	args: {
-		children: 'Button',
+		children: 'Primary',
+	},
+};
+
+export const Outline: Story = {
+	args: {
+		children: 'Outline',
+		variant: 'outline',
 	},
 };
