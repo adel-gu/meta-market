9f20c34968a7098dadf3e5039329d9c3f925faab
adel <adeelguitoun@gmail.com>
2024-11-04 17:43:44 +0100

    Test mint-ui on container app

--- apps/container/tailwind.config.js
+++ apps/container/tailwind.config.js
@@ -1,19 +1,23 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
-const baseConfig = require('../../tailwind.config.base.js');
+// const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+// const { join } = require('path');
+// const baseConfig = require('../../tailwind.config.base.js');
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	presets: [baseConfig],
-	content: [
-		join(
-			__dirname,
-			'{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-		),
-		...createGlobPatternsForDependencies(__dirname),
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+// 	// presets: [baseConfig],
+// 	content: [
+// 		join(
+// 			__dirname,
+// 			'{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+// 		),
+// 		...createGlobPatternsForDependencies(__dirname),
+// 	],
+// 	theme: {
+// 		extend: {},
+// 	},
+// 	plugins: [],
+// };
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { buildConfig } from '../../packages/mint-ui/tailwind.config';
+
+export default buildConfig(__dirname);
