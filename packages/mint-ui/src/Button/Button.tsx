ac769a727f28a4c8f8d5d8f7499ea9f31209a6ce
adel <adeelguitoun@gmail.com>
2024-11-04 08:37:16 +0100

    Configure tailwind style guide

--- packages/mint-ui/src/Button/Button.tsx
+++ packages/mint-ui/src/Button/Button.tsx
@@ -1,7 +1,7 @@
 export const Button = () => {
 	return (
 		<button
-			className='text-md rounded-xl bg-green-500 px-3 py-2 font-semibold text-white'
+			className='text-md rounded-xl px-3 py-2 font-semibold text-white bg-gradient-1'
 			type='button'>
 			Button
 		</button>
---
+++ tailwind.config.base.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.875rem",
+        tablet: "4.5rem",
+        desktop: "7.188rem"
+      }
+    },
+    screens: {
+      tablet: "960px",
+      desktop: "1248px"
+    },
+    colors: {
+      "bg-primary": "#2b2b2b",
+      "bg-secondary": "#3b3b3b",
+      "caption-label-clr": "#858584",
+      "text-clr": "#fff",
+      "cta": "#a259ff"
+
+    },
+    fontFamily: {
+      'space-mono': '"Space Mono", serif',
+      'work-sans': '"Work Sans", serif',
+    },
+    fontSize: {
+      "sm": ["12px", {lineHeight: "19.2px"}],
+      "base": ["16px", {lineHeight: "25.6px"}],
+      "md": ["22px", {lineHeight: "35.2px"}],
+      "lg": ["28px", {lineHeight: "44.8px"}],
+      "xl": ["38px", {lineHeight: "60.8px"}],
+      "2xl": ["51px", {lineHeight: "81.6px"}],
+      "3xl": ["67px", {lineHeight: "107.2px"}],
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-1": "linear-gradient(-68deg, #a259ff 0%, #ff6250 100%)",
+        "gradient-2": "linear-gradient(-36deg, #a259ff 0%, #377df7 100%)",
+      }
+    }
+  }
+};
