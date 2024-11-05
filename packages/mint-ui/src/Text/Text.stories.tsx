6ce2748b32dd304008db9bd8292d1c7af011dbe1
adel <adeelguitoun@gmail.com>
2024-11-05 16:54:08 +0100

    Added weight prop to text component

--- packages/mint-ui/src/Text/Text.stories.tsx
+++ packages/mint-ui/src/Text/Text.stories.tsx
@@ -8,58 +8,46 @@ const meta: Meta<typeof Text> = {
 	parameters: {
 		layout: 'centered',
 	},
+	argTypes: {
+		as: {
+			options: ['h1', 'h2', 'h3', 'h4', 'p'],
+			control: { type: 'inline-radio' },
+		},
+		variant: {
+			options: ['default', 'caption'],
+			control: { type: 'inline-radio' },
+		},
+		size: {
+			options: ['1', '2', '3', '4', '5', '6', '7'],
+			control: { type: 'inline-radio' },
+		},
+	},
 };
 export default meta;
+
 type Story = StoryObj<typeof Text>;
 
 export const Heading: Story = {
 	args: {
 		as: 'h1',
 		children: 'Heading',
-	},
-	argTypes: {
-		as: {
-			options: ['h1', 'h2', 'h3', 'h4', 'h5'],
-			control: { type: 'inline-radio' },
-		},
-		size: {
-			options: ['md', 'lg', 'xl', 'xxl', 'xxxl'],
-			control: { type: 'inline-radio' },
-		},
+		size: '7',
 	},
 };
 
 export const Paragraph: Story = {
 	args: {
 		as: 'p',
-		children: 'Paragraph',
-	},
-	argTypes: {
-		as: {
-			options: ['p'],
-		},
-		size: {
-			options: ['sm', 'base'],
-			control: { type: 'inline-radio' },
-		},
+		children: 'This is a paragraph text.',
+		size: '2',
 	},
 };
 
 export const Caption: Story = {
 	args: {
 		as: 'p',
-		children: 'Caption',
-	},
-	argTypes: {
-		as: {
-			options: ['p'],
-		},
-		variant: {
-			options: ['caption'],
-		},
-		size: {
-			options: ['sm', 'base'],
-			control: { type: 'inline-radio' },
-		},
+		children: 'This is a caption text.',
+		variant: 'caption',
+		size: '1',
 	},
 };
