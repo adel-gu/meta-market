298d9eccdd1708411ed0ab5254bcda4d90a62030
adel <adeelguitoun@gmail.com>
2024-11-05 16:34:56 +0100

    Added Text component and it's story

--- packages/mint-ui/.storybook/preview.ts
+++ packages/mint-ui/.storybook/preview.ts
@@ -1,1 +1,17 @@
+import { Preview } from '@storybook/react';
+import { themes } from '@storybook/theming';
+
 import '../src/global.css';
+
+const preview: Preview = {
+	parameters: {
+		docs: {
+			theme: themes.dark,
+		},
+		themes: {
+			current: themes.dark,
+		},
+	},
+};
+
+export default preview;
