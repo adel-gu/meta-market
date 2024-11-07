98b12f778d5c96b7571b458839bb5d3deaee4858
adel <adeelguitoun@gmail.com>
2024-11-07 18:57:40 +0100

    Added avatar sm

--- packages/mint-ui/src/Avatar/avatar.tsx
+++ packages/mint-ui/src/Avatar/avatar.tsx
@@ -1,18 +1,36 @@
 import * as React from 'react';
 import * as AvatarPrimitive from '@radix-ui/react-avatar';
+import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '../cn';
 
+const avatarVariants = cva(
+	'relative flex shrink-0 overflow-hidden rounded-full',
+	{
+		variants: {
+			size: {
+				default:
+					'h-[60px] w-[60px] rounded-full text-2xl desktop:h-[120px] desktop:w-[120px]',
+				sm: 'h-[24px] w-[24px] text-[8px]',
+			},
+		},
+		defaultVariants: {
+			size: 'default',
+		},
+	}
+);
+
+interface AvatarProps
+	extends React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>,
+		VariantProps<typeof avatarVariants> {}
+
 const Avatar = React.forwardRef<
 	React.ElementRef<typeof AvatarPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
->(({ className, ...props }, ref) => (
+	AvatarProps
+>(({ className, size, ...props }, ref) => (
 	<AvatarPrimitive.Root
 		ref={ref}
-		className={cn(
-			'relative flex h-[60px] w-[60px] shrink-0 overflow-hidden rounded-full desktop:h-[120px] desktop:w-[120px]',
-			className
-		)}
+		className={cn(avatarVariants({ size, className }))}
 		{...props}
 	/>
 ));
@@ -37,7 +55,7 @@ const AvatarFallback = React.forwardRef<
 	<AvatarPrimitive.Fallback
 		ref={ref}
 		className={cn(
-			'flex h-full w-full items-center justify-center rounded-full bg-foreground-secondary text-foreground-primary desktop:text-2xl',
+			'flex h-full w-full items-center justify-center rounded-full bg-foreground-secondary text-foreground-primary',
 			className
 		)}
 		{...props}
@@ -45,4 +63,4 @@ const AvatarFallback = React.forwardRef<
 ));
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
 
-export { Avatar, AvatarImage, AvatarFallback };
+export { Avatar, AvatarImage, AvatarFallback, AvatarProps };
