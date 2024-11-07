1bed10a33edfebbc1d55e6274c3cf898c15f3468
adel <adeelguitoun@gmail.com>
2024-11-07 21:38:28 +0100

    Added export for component

--- packages/mint-ui/src/index.ts
+++ packages/mint-ui/src/index.ts
@@ -2,7 +2,10 @@ import './global.css';
 
 export * from './AspectRatio';
 export * from './Avatar';
+export * from './Badge';
 export * from './Button';
 export * from './Image';
+export * from './Input';
+export * from './Separator';
 export * from './Skeleton';
 export * from './Text';
