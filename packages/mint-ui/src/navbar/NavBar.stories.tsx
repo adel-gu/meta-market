4d462ad2e3ef69b2e64566021e5658888e497d36
adel <adeelguitoun@gmail.com>
2024-11-10 16:10:10 +0100

    Create navBar stories

---
+++ packages/mint-ui/src/navbar/NavBar.stories.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { Logo, Menu } from '@meta-market/mint-icons';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from '../Button';
+import { NavBar, NavBrand, NavCollapse, NavToggle } from './navbar';
+
+const meta: Meta<typeof NavBar> = {
+	title: 'Layouts/NavBar',
+	component: NavBar,
+};
+export default meta;
+type Story = StoryObj<typeof NavBar>;
+
+export const Primary: Story = {
+	args: {},
+	render: () => {
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		const [isOpen, setIsOpen] = useState(false);
+
+		return (
+			<NavBar>
+				<NavBrand>
+					<Logo className='text-3xl text-primary' width={240} />
+				</NavBrand>
+				<NavToggle onClick={() => setIsOpen(!isOpen)} variant='transparent'>
+					<Menu />
+				</NavToggle>
+				<NavCollapse isOpen={isOpen}>
+					<a href='#home' className='hover:text-primary'>
+						Home
+					</a>
+					<a href='#about' className='hover:text-primary'>
+						About
+					</a>
+					<a href='#services' className='hover:text-primary'>
+						Services
+					</a>
+					<a href='#contact' className='hover:text-primary'>
+						Contact
+					</a>
+					<Button>Sign Up</Button>
+				</NavCollapse>
+			</NavBar>
+		);
+	},
+};
