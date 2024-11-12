dd15b7d7a564852b76c86bde7d3290124cf7c742
adel <adeelguitoun@gmail.com>
2024-11-12 11:47:17 +0100

    Added CollectionCard stories

--- packages/mint-ui/src/Card/Card.stories.tsx
+++ packages/mint-ui/src/Card/Card.stories.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Avatar, AvatarFallback, AvatarImage } from '../Avatar';
+import { Grid } from '../Grid/grid';
 import { Image } from '../Image/image';
 import { Skeleton } from '../Skeleton';
 import { Text } from '../Text/text';
@@ -82,6 +83,62 @@ export const HeroCard: Story = {
 					</Card>
 				)}
 			</div>
+		);
+	},
+};
+
+export const CollectionCard: Story = {
+	args: {},
+	render: () => {
+		return (
+			<Card className='space-y-4 rounded-none desktop:ml-auto desktop:w-[330px]'>
+				<Grid columns={3} gap='md' className='w-full'>
+					<div className='col-span-3 h-[330px]'>
+						<Image
+							src='https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80'
+							alt='NFT'
+							width={'100%'}
+							effect='blur'
+							className='h-full w-full rounded-large object-cover'
+						/>
+					</div>
+
+					<Image
+						src='https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80'
+						alt='NFT'
+						width={'100%'}
+						effect='blur'
+						className='h-[100px] rounded-large object-cover'
+					/>
+					<Image
+						src='https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80'
+						alt='NFT'
+						width={'100%'}
+						effect='blur'
+						className='h-[100px] rounded-large object-cover'
+					/>
+					<div className='flex h-[100px] w-full items-center justify-center rounded-large bg-primary object-cover text-foreground-primary'>
+						1024+
+					</div>
+				</Grid>
+				<CardHeader className='p-0'>
+					<CardTitle>
+						<Text as='p' size='6' weight='semibold' className='desktop:text-md'>
+							Space Walking
+						</Text>
+					</CardTitle>
+					<CardDescription className='flex items-center gap-3'>
+						<Avatar size='sm'>
+							<AvatarImage
+								src='https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80'
+								alt='avatar'
+							/>
+							<AvatarFallback>AV</AvatarFallback>
+						</Avatar>
+						<Text>Avatar</Text>
+					</CardDescription>
+				</CardHeader>
+			</Card>
 		);
 	},
 };
