94ad1c5694e3d4fa1b13bbffec98dabe7f5980fb
adel <adeelguitoun@gmail.com>
2024-11-12 18:41:44 +0100

    Created CreatorsSection component

--- apps/landing/src/app/pages/Home.tsx
+++ apps/landing/src/app/pages/Home.tsx
@@ -1,4 +1,5 @@
 import CollectionSection from '../components/CollectionSection';
+import CreatorsSection from '../components/CreatorsSection';
 import HeroSection from '../components/HeroSection';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
 		<div>
 			<HeroSection />
 			<CollectionSection />
+			<CreatorsSection />
 		</div>
 	);
 };
