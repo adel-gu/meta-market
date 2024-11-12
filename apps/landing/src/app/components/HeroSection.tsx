da7a1f747affde7f66c046de8ccae3f66466cd6c
adel <adeelguitoun@gmail.com>
2024-11-12 09:20:02 +0100

    Fix container padding size

--- apps/landing/src/app/components/HeroSection.tsx
+++ apps/landing/src/app/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import HeroRight from './HeroRight';
 
 const HeroSection = () => {
 	return (
-		<section className='container tablet:p-0'>
+		<section className='container py-10 tablet:py-20 tablet:px-0'>
 			<div className='flex flex-col items-center gap-8 tablet:flex-row'>
 				<HeroLeft />
 				<HeroRight />
