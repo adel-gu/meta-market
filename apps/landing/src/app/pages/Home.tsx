03965b318bb158aceeae44095a3b5056304d74b6
adel <adeelguitoun@gmail.com>
2024-11-14 11:51:16 +0100

    Created WeeklyDigest section

--- apps/landing/src/app/pages/Home.tsx
+++ apps/landing/src/app/pages/Home.tsx
@@ -5,6 +5,7 @@ import CreatorsSection from '../components/CreatorsSection';
 import DiscoverNftsSection from '../components/DiscoverNftsSection';
 import HeroSection from '../components/HeroSection';
 import HowItWorks from '../components/HowItWorks';
+import WeeklyDigest from '../components/WeeklyDigest';
 
 const Home = () => {
 	return (
@@ -16,6 +17,7 @@ const Home = () => {
 			<DiscoverNftsSection />
 			<AuctionSection />
 			<HowItWorks />
+			<WeeklyDigest />
 		</div>
 	);
 };
