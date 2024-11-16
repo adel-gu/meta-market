5bdc536b2f7c47b3d96cf7e48b2c8f078497fa6d
adel <adeelguitoun@gmail.com>
2024-11-16 11:48:38 +0100

    Create ConnectWallet page

--- apps/auth/src/app/pages/ConnectWallet.tsx
+++ apps/auth/src/app/pages/ConnectWallet.tsx
@@ -1,6 +1,49 @@
+import { Text, Button } from '@meta-market/mint-ui';
+import { Metamask, Coinbase, WalletConnect } from '@meta-market/mint-icons';
+
 const ConnectWallet = () => {
-  return (
-    <div>ConnectWallet</div>
-  )
-}
-export default ConnectWallet
+	return (
+		<div className='container flex flex-col justify-center py-8 gap-8 tablet:px-16'>
+			<div className='space-y-4 text-center tablet:text-left'>
+				<Text
+					as='h2'
+					size='2'
+					weight='semibold'
+					className='leading-tight tablet:leading-none desktop:leading-none desktop:w-4/5'>
+					Connect wallet
+				</Text>
+				<Text className='desktop:text-md desktop:w-4/5'>
+					Choose a wallet you want to connect. There are several wallet
+					providers.
+				</Text>
+			</div>
+			<div className='w-fit space-y-5'>
+				<Button
+					className='flex items-center justify-start gap-3 w-full text-foreground-primary'
+					variant='outline'>
+					<span>
+						<Metamask />
+					</span>
+					<span>Metamask</span>
+				</Button>
+				<Button
+					className='flex items-center justify-start gap-3 w-full text-foreground-primary'
+					variant='outline'>
+					<span>
+						<WalletConnect />
+					</span>
+					<span>Wallet Connect</span>
+				</Button>
+				<Button
+					className='flex items-center justify-start gap-3 w-full text-foreground-primary'
+					variant='outline'>
+					<span>
+						<Coinbase />
+					</span>
+					<span>Coinbase</span>
+				</Button>
+			</div>
+		</div>
+	);
+};
+export default ConnectWallet;
