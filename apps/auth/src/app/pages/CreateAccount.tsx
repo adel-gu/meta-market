02e885ffbf5e211239ead3f088144bea0976dc01
adel <adeelguitoun@gmail.com>
2024-11-16 11:29:33 +0100

    Created CreateAccount component

--- apps/auth/src/app/pages/CreateAccount.tsx
+++ apps/auth/src/app/pages/CreateAccount.tsx
@@ -1,6 +1,35 @@
+import { Text, Input, Button } from '@meta-market/mint-ui';
+
 const CreateAccount = () => {
-  return (
-    <div>CreateAccount</div>
-  )
-}
-export default CreateAccount
+	return (
+		<div className='container flex flex-col justify-center py-8 gap-8 tablet:px-16'>
+			<div className='space-y-4 text-center tablet:text-left'>
+				<Text
+					as='h2'
+					size='2'
+					weight='semibold'
+					className='leading-tight tablet:leading-none desktop:leading-none desktop:w-4/5'>
+					Create account
+				</Text>
+				<Text className='desktop:text-md desktop:w-4/5'>
+					Welcome! enter your details and start creating, collecting and selling
+					NFTs.
+				</Text>
+			</div>
+			<form className='flex flex-col gap-4'>
+				<Input className='w-full' variant='white' placeholder='Username' />
+				<Input className='w-full' variant='white' placeholder='Email Address' />
+				<Input className='w-full' variant='white' placeholder='Password' />
+				<Input
+					className='w-full'
+					variant='white'
+					placeholder='Confirm Password'
+				/>
+				<Button className='flex items-center gap-3 w-full' type='submit'>
+					Create account
+				</Button>
+			</form>
+		</div>
+	);
+};
+export default CreateAccount;
