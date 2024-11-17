c519fe2ed81d47bc8a8f8a6d0d5b46ae66626e64
adel <adeelguitoun@gmail.com>
2024-11-17 16:03:22 +0100

    Created artis page

--- apps/profiles/src/app/Pages/ArtistPage.tsx
+++ apps/profiles/src/app/Pages/ArtistPage.tsx
@@ -1,6 +1,18 @@
+import ProfileBackground from '../../assets/profile-background.png';
+import ArtistProfileBio from '../components/ArtistProfileBio';
+import NftsTabs from '../components/NftsTabs';
+
 const ArtistPage = () => {
-  return (
-    <div>ArtistPage</div>
-  )
-}
-export default ArtistPage
+	return (
+		<div>
+			<div
+				className='relative bg-center bg-cover h-[250px] tablet:h-[280px] desktop:h-[320px]'
+				style={{ backgroundImage: `url(${ProfileBackground})` }}>
+				<div className='absolute inset-0 z-0 bg-gradient-to-t	from-primary/85' />
+			</div>
+			<ArtistProfileBio />
+			<NftsTabs />
+		</div>
+	);
+};
+export default ArtistPage;
