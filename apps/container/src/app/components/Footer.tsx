e74ecef3b9a0b3d73eccd56866b4f9d6c978e3d4
adel <adeelguitoun@gmail.com>
2024-11-17 20:58:38 +0100

    Updated footer background color

--- apps/container/src/app/components/Footer.tsx
+++ apps/container/src/app/components/Footer.tsx
@@ -29,60 +29,62 @@ const links = [
 
 const Footer = () => {
 	return (
-		<footer className='container py-10 tablet:py-20'>
-			<div className='space-y-8 tablet:flex tablet:items-start tablet:justify-between tablet:space-y-0 '>
-				<div className='space-y-3 tablet:mt-1'>
-					<Link to='/'>
-						<Logo className='text-3xl text-primary' width={240} />
-					</Link>
-					<Text>NFT marketplace UI created with Anima for Figma.</Text>
-					<Text>Join our community.</Text>
-					<div className='flex gap-4'>
-						<DiscordLogo />
-						<YoutubeLogo />
-						<TwitterLogo />
-						<InstagramLogo />
+		<footer className='bg-bg-secondary'>
+			<div className='container py-10 tablet:py-20'>
+				<div className='space-y-8 tablet:flex tablet:items-start tablet:justify-between tablet:space-y-0 '>
+					<div className='space-y-3 tablet:mt-1'>
+						<Link to='/'>
+							<Logo className='text-3xl text-primary' width={240} />
+						</Link>
+						<Text>NFT marketplace UI created with Anima for Figma.</Text>
+						<Text>Join our community.</Text>
+						<div className='flex gap-4'>
+							<DiscordLogo />
+							<YoutubeLogo />
+							<TwitterLogo />
+							<InstagramLogo />
+						</div>
 					</div>
-				</div>
-				<div className='space-y-3'>
-					<Text as='p' size='3' weight='semibold'>
-						Explore
-					</Text>
-					<div className='flex flex-col space-y-3'>
-						{links.map((link) => (
-							<Link
-								key={link.id}
-								to={link.path}
-								className={`desktop:hover:underline`}>
-								{link.title}
-							</Link>
-						))}
+					<div className='space-y-3'>
+						<Text as='p' size='3' weight='semibold'>
+							Explore
+						</Text>
+						<div className='flex flex-col space-y-3'>
+							{links.map((link) => (
+								<Link
+									key={link.id}
+									to={link.path}
+									className={`desktop:hover:underline`}>
+									{link.title}
+								</Link>
+							))}
+						</div>
 					</div>
-				</div>
-				<div className='mt-0 space-y-3 p-0'>
-					<Text as='p' size='3' weight='semibold'>
-						Join our weekly digest
-					</Text>
-					<Text>
-						Get exclusive promotions & updates straight to your inbox.
-					</Text>
-					<div className='flex flex-col gap-4 p-0 '>
-						<Input
-							className='w-full'
-							variant='white'
-							placeholder='Enter your email here'
-						/>
-						<Button className='flex items-center gap-3 w-full' type='submit'>
-							<EnvelopeSimple />
+					<div className='mt-0 space-y-3 p-0'>
+						<Text as='p' size='3' weight='semibold'>
+							Join our weekly digest
+						</Text>
+						<Text>
+							Get exclusive promotions & updates straight to your inbox.
+						</Text>
+						<div className='flex flex-col gap-4 p-0 '>
+							<Input
+								className='w-full'
+								variant='white'
+								placeholder='Enter your email here'
+							/>
+							<Button className='flex items-center gap-3 w-full' type='submit'>
+								<EnvelopeSimple />
 
-							<span>Subscribe</span>
-						</Button>
+								<span>Subscribe</span>
+							</Button>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className='mt-16 space-y-3 '>
-				<Separator />
-				<Text>Ⓒ NFT Market. Use this template freely.</Text>
+				<div className='mt-16 space-y-3 '>
+					<Separator />
+					<Text>Ⓒ NFT Market. Use this template freely.</Text>
+				</div>
 			</div>
 		</footer>
 	);
